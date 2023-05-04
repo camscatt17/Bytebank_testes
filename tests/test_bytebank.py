bea1bcc8d59dda24441c3b035444c7f06f976cc7
Ellejeane Camila Santos <camscatt17@gmail.com>
2023-05-04 11:13:10 -0300

    Refatoração utilizando fixtures.

--- tests/test_bytebank.py
+++ tests/test_bytebank.py
@@ -1,20 +1,23 @@
+import pytest as pytest
+
 from codigo.bytebank import Funcionario
 
 class TestClass:
-    def test_quando_idade_recebe_13_03_2000_deve_retornar_22(self):
-        entrada = '13/03/2000' #Given- Contexto
-        esperado = 23
 
-        funcionario_teste = Funcionario('Teste', entrada, 1111)
-        resultado = funcionario_teste.idade() #When-ação
+    @pytest.fixture
+    def funcionario(self):
+        return Funcionario('Camila Santos', '17/01/1997', 1111)
 
-        assert resultado == esperado #Then-desfecho
 
-    def test_quando_sobrenome_recebe_Camila_Santos_deve_retornar_Santos(self):
-        entrada = ' Camila Santos ' #Given
+    def test_quando_idade_recebe_13_03_2000_deve_retornar_22(self, funcionario):
+        camila = funcionario #Given
+        esperado = 26
+        resultado = camila.idade() #When
+        assert resultado == esperado #Then
+
+    def test_quando_sobrenome_recebe_Camila_Santos_deve_retornar_Santos(self, funcionario):
+        camila = funcionario #Given
         esperado = 'Santos'
-
-        camila = Funcionario(entrada, '11/11/2000', 1111)
         resultado = camila.sobrenome() #When
 
-        assert resultado == esperado
+        assert resultado == esperado #Then
